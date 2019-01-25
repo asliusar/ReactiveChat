8e60b879cc977b691dcd03e4efeeb928debc42be
Sliusar <slian01@ca.com>
2019-01-25 23:31:38 +0100

    add a support for Enter button

--- frontend/src/chat/components/common/InputBlock.tsx
+++ frontend/src/chat/components/common/InputBlock.tsx
@@ -7,17 +7,30 @@ interface InputBlockProps {
     buttonText: string,
     inputPlaceholder?: string
 }
+
 // TODO move it to a shared module
 const InputBlock = (props: InputBlockProps & WithStyles<any>) => {
     const [text, setText] = React.useState('');
+
+    const onSubmit = (text: string) => {
+        props.onSubmit(text);
+        setText('');
+    }
+
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            onSubmit(text);
+        }
+    }
 
     return (
         <div className={props.classes.block}>
             <input type='text' name='text' className={props.classes.input} 
                 value={text} placeholder={props.inputPlaceholder} 
                 onChange={(event) => setText(event.target.value)}
+                onKeyPress={handleKeyPress}
             />
-            <button className={props.classes.button} onClick={() => props.onSubmit(text)}>
+            <button className={props.classes.button} onClick={() => onSubmit(text)}>
                 {props.buttonText}
             </button>
         </div>
